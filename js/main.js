57288d198eb8cbbca6840c6449bf0720a4758f3e
S_Yanushenko <107427237+Rinzler7@users.noreply.github.com>
2022-07-11 13:04:28 +0300

    Layout AVTR
    Layout AVTR

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.design-slider').slick({
+        dots: false,
+        slidesToShow: 4,
+        variableWidth: true,
+        prevArrow: '<img src="../Project_1/images/arrow-left.svg" alt="" class="arrow arrow-left">',
+        nextArrow: '<img src="../Project_1/images/arrow-right.svg" alt="" class="arrow arrow-right">'
+    });
+});
